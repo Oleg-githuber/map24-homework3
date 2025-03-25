df251327b53eb1eb8c97ce961d8df351b2fb5de3
Oleg-githuber <oilminer@mail.ru>
2025-03-25 15:48:57 +0500

    Homework

---
+++ Homework1/Homework1.cpp
@@ -0,0 +1,107 @@
+﻿/*
+Реализуйте алгоритм сортировки выбором.
+Сортировка выбором состоит в том, что в неупорядоченном списке находитсянаименьший элемент. 
+Выбранный в исходном списке минимальный элементзаписывается на i-е место исходного списка (i=1,2,…,п), 
+а элемент с i-го места— на место выбранного. При этом очевидно, что уже упорядоченныеэлементы, 
+а они будут расположены, начиная с первого места, исключаются издальнейшей сортировки, поэтому длина списка, участвующего в каждом
+
+Поиск минимального элемента реализуйте в виде асинхронной задачи.
+
+Результат должен возвращаться в основной поток через связку std::promise-std::future.
+*/
+
+#include <iostream>
+#include <future>
+//#include <thread>
+
+constexpr size_t arrSize = 5;
+std::atomic<int> arr[arrSize]{ 2, 4, 1, 0, 3 };     // Сделал массив глобальным, чтобы уменьшить число аргументов std::thread
+
+/// <summary>
+/// Поиск индекса минимального элемента
+/// </summary>
+/// <param name="index">Начальный индекс поиска минимума</param>
+/// <param name="promiseMin">std::promise</param>
+void findMin(size_t index, std::promise<size_t>& promiseMin)
+{
+    size_t minIndex{ index };
+    for (size_t i{index + 1}; i < arrSize; ++i)
+    {
+        if (arr[i].load() < arr[minIndex].load())
+        {
+            minIndex = i;
+        }
+    }
+    promiseMin.set_value(minIndex);
+}
+
+/// <summary>
+/// Перестановка элементов в массиве
+/// </summary>
+/// <param name="index1">Индекс первого элемента</param>
+/// <param name="index2">Индекс второго элемента</param>
+void swapElements(size_t index1, size_t index2)
+{
+    if (index1 == index2)
+    {
+        return;     // Ничего не меняем, если минимальный элемент находится в начале
+    }
+    int temp{ arr[index1].load()};
+    arr[index1].store(arr[index2].load());
+    arr[index2].store(temp);
+}
+
+/// <summary>
+/// Сортировка выбором части массива
+/// </summary>
+/// <param name="index">Начальный индекс поиска минимума</param>
+void asyncChoiseSort(size_t index)
+{
+    if (index >= arrSize)
+    {
+        return;
+    }
+
+    std::promise<size_t> promiseMin{};
+    std::future<size_t> futureMin{ promiseMin.get_future() };
+    std::thread myThread(findMin, index, std::ref(promiseMin));
+    //futureMin.wait();
+    swapElements(index, futureMin.get());
+    myThread.join();
+    asyncChoiseSort(index + 1);
+}
+
+/// <summary>
+/// Сортировка выбором всего массива
+/// </summary>
+void asyncChoiseSort()
+{
+    asyncChoiseSort(0);
+}
+
+/// <summary>
+/// Вывод массива на экран
+/// </summary>
+void printArray()
+{
+    for (size_t i{}; i < arrSize; ++i)
+    {
+        std::cout << arr[i].load() << "  ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    
+
+    std::cout << "Array before sorting: ";
+    printArray();
+
+    asyncChoiseSort();
+
+    std::cout << "Array after sorting:  ";
+    printArray();
+
+    return EXIT_SUCCESS;
+}
---
+++ Homework2/Homework2.cpp
@@ -0,0 +1,75 @@
+﻿/*
+Реализуйте параллельный вариант оператора for_each.
+
+Функция должна принимать итераторы на начало и конец контейнера иссылку, обрабатывающую функцию.
+
+При реализации нужно рекурсивно разделить контейнер на несколько блокови для каждого запустить отдельную задачу, 
+применяющую обрабатывающуюфункцию к части контейнера.
+*/
+
+#include <iostream>
+#include <future>
+#include <array>
+
+std::array<int, 5> arr{ 4, 2, 1, 0, 3 };    // Глобальный массив
+
+/// <summary>
+/// Вывод массива в консоль
+/// </summary>
+/// <typeparam name="T">Тип значений</typeparam>
+/// <typeparam name="Iterator">Итератор</typeparam>
+/// <param name="first">Начало массива</param>
+/// <param name="last">Конец массива</param>
+template<typename T, typename Iterator>
+void printArray(Iterator first, Iterator last)
+{
+    int64_t size{ std::distance(first, last) };
+    Iterator iterator{ first };
+    while (iterator != last)
+    {
+        std::cout << *(iterator++) << "  ";
+    }
+    std::cout << std::endl;
+}
+
+/// <summary>
+/// Функция параллельного суммирования элементов массива
+/// </summary>
+/// <typeparam name="Iterator">Итератор</typeparam>
+/// <typeparam name="T">Тип значений</typeparam>
+/// <param name="first">Начало массива</param>
+/// <param name="last">Конец массива</param>
+/// <returns>Сумма элементов массива</returns>
+template<typename Iterator, typename T>
+T parallel_fun(Iterator first, Iterator last)
+{
+    int64_t size{ std::distance(first, last) };
+    if (size > 2)
+    {
+        Iterator iterator{ first };
+        std::advance(iterator, size / 2);
+        std::future<T> leftInit{ std::async(parallel_fun<Iterator, T>,first, iterator) };
+        std::future<T> rightInit{ std::async(parallel_fun<Iterator, T>, iterator, last) };
+        return leftInit.get() + rightInit.get();
+    }
+    else
+    {
+        T sum{};
+        Iterator iterator = first;
+        while (iterator != last)
+        {
+            sum += *(iterator++);
+        }
+        return sum;
+    }
+}
+
+int main()
+{
+    
+    printArray<int, std::array<int, 5>::iterator>(arr.begin(), arr.end());
+
+    std::cout << "sum = " << parallel_fun<std::array<int, 5>::iterator, int>(arr.begin(), arr.end());
+
+    return EXIT_SUCCESS;
+}
