a8ec1524acce6e113cdab6fd23eb1b783f9d8b30
Oleg-githuber <oilminer@mail.ru>
2025-03-26 01:18:17 +0500

    change Task2

--- Homework2/Homework2.cpp
+++ Homework2/Homework2.cpp
@@ -14,62 +14,68 @@
 std::array<int, 5> arr{ 4, 2, 1, 0, 3 };    // Глобальный массив
 
 /// <summary>
-/// Вывод массива в консоль
+/// Вычисление суммы элементов массива
 /// </summary>
-/// <typeparam name="T">Тип значений</typeparam>
+/// <typeparam name="T">Тип элементов массива</typeparam>
 /// <typeparam name="Iterator">Итератор</typeparam>
 /// <param name="first">Начало массива</param>
 /// <param name="last">Конец массива</param>
+/// <returns>Сумма</returns>
 template<typename T, typename Iterator>
-void printArray(Iterator first, Iterator last)
+T calculateSum(Iterator first, Iterator last)
 {
-    int64_t size{ std::distance(first, last) };
-    Iterator iterator{ first };
-    while (iterator != last)
-    {
-        std::cout << *(iterator++) << "  ";
-    }
-    std::cout << std::endl;
+	T sum{};
+	Iterator iterator{ first };
+	while (iterator != last)
+	{
+		sum += *(iterator++);
+	}
+	return sum;
 }
 
 /// <summary>
-/// Функция параллельного суммирования элементов массива
+/// Параллельный foreach
 /// </summary>
+/// <typeparam name="T">Тип элементов массива</typeparam>
 /// <typeparam name="Iterator">Итератор</typeparam>
-/// <typeparam name="T">Тип значений</typeparam>
 /// <param name="first">Начало массива</param>
 /// <param name="last">Конец массива</param>
-/// <returns>Сумма элементов массива</returns>
-template<typename Iterator, typename T>
-T parallel_fun(Iterator first, Iterator last)
+/// <param name="func">Указатель на функцию вычисления суммы</param>
+/// <returns>Возврат суммы элементов</returns>
+template<typename T, typename Iterator>
+T parallel_foreach(Iterator first, Iterator last, T (*func)(Iterator, Iterator))
 {
-    int64_t size{ std::distance(first, last) };
-    if (size > 2)
-    {
-        Iterator iterator{ first };
-        std::advance(iterator, size / 2);
-        std::future<T> leftInit{ std::async(parallel_fun<Iterator, T>,first, iterator) };
-        std::future<T> rightInit{ std::async(parallel_fun<Iterator, T>, iterator, last) };
-        return leftInit.get() + rightInit.get();
-    }
-    else
-    {
-        T sum{};
-        Iterator iterator = first;
-        while (iterator != last)
-        {
-            sum += *(iterator++);
-        }
-        return sum;
-    }
+	int64_t size{ std::distance(first, last) };
+	if (size > 2)
+	{
+		Iterator iterator{ first };
+		std::advance(iterator, size / 2);
+		std::future<T> left{ std::async(parallel_foreach<T, Iterator>, first, iterator, func) };
+		std::future<T> right{ std::async(parallel_foreach<T, Iterator>, iterator, last, func) };
+		return left.get() + right.get();
+	}
+	else
+	{
+		return func(first, last);
+	}
+}
+
+template<typename T, typename Iterator>
+void printArray(Iterator first, Iterator last)
+{
+	std::cout << "Array: ";
+	Iterator iterator{ first };
+	while (iterator != last)
+	{
+		std::cout << *(iterator++) << "  ";
+	}
+	std::cout << std::endl;
 }
 
 int main()
 {
-    
-    printArray<int, std::array<int, 5>::iterator>(arr.begin(), arr.end());
-
-    std::cout << "sum = " << parallel_fun<std::array<int, 5>::iterator, int>(arr.begin(), arr.end());
+	printArray<int, std::array<int, 5>::iterator>(arr.begin(), arr.end());
+	std::cout << "sum = " << parallel_foreach<int, std::array<int, 5>::iterator>(arr.begin(), arr.end(), &calculateSum);
 
     return EXIT_SUCCESS;
 }
